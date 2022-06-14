ed7a00743077cee1634700782d4aff7eea93f1ce
ABNER VIDAL <89116961+abnervidal@users.noreply.github.com>
2022-06-14 17:00:54 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,15 @@
+
+function mudaFoto (foto) {
+	document.getElementById("icone").src = foto;
+}
+function calc_total() {
+	var qtd = parseInt(document.getElementById('quant').value);
+	tot = qtd * 1500;
+	document.getElementById('total').value = tot;
+}
+class MobileNavbar {
+	constructor(barra, check) {
+		this.barra
+		this.check
+	}
+}
